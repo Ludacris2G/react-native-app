674d0aff829c04162c6cea3471bf674a95b498d2
Liviu <liviutoma946@gmail.com>
2024-01-24 23:58:23 +0200

    layout fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,17 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  });
   return <Stack />;
 };
 
